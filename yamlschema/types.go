0de1c4d6f0891c817f8a015689e6cbb030df1db4
Kory Prince <korylprince@gmail.com>
2025-04-24 20:29:09 -0500

    Add BooleanIntegerNumberString type

--- yamlschema/types.go
+++ yamlschema/types.go
@@ -121,14 +121,11 @@ func (in *IntegerNumberString) Value() any {
 }
 
 func (in *IntegerNumberString) UnmarshalYAML(data []byte) error {
-	// TODO: currently Apple's schema PayloadKeys.Default schema is inconsistent with it's use.
-	// The schema doesn't allow Default to have the type boolean, but it uses boolean types in some schemas (mdm/checkin/getbootstraptoken.yaml)
-
-	// verify string isn't a bool
-	// var b bool
-	// if err := yaml.Unmarshal(data, &b); err == nil {
-	// 	return ErrUnexpectedBool
-	// }
+	//verify string isn't a bool
+	var b bool
+	if err := yaml.Unmarshal(data, &b); err == nil {
+		return ErrUnexpectedBool
+	}
 
 	if bytes.Contains(data, []byte{'.'}) {
 		var f float64
@@ -169,6 +166,116 @@ func NewIntegerNumberString[T int64 | float64 | string](v T) *IntegerNumberStrin
 		return &IntegerNumberString{float64: &val}
 	case string:
 		return &IntegerNumberString{string: &val}
+	default:
+		panic(fmt.Sprintf("unexpected type %T", val))
+	}
+}
+
+// BooleanIntegerNumberString holds a bool, int64, float64, or a string
+type BooleanIntegerNumberString struct {
+	*bool
+	*int64
+	*float64
+	*string
+}
+
+func (in *BooleanIntegerNumberString) IsBool() bool {
+	return in.bool != nil
+}
+
+func (in *BooleanIntegerNumberString) IsInt64() bool {
+	return in.int64 != nil
+}
+
+func (in *BooleanIntegerNumberString) IsFloat64() bool {
+	return in.float64 != nil
+}
+
+func (in *BooleanIntegerNumberString) IsString() bool {
+	return in.string != nil
+}
+
+// Bool returns the underlying bool. Bool will panic if the underlying type is not bool
+func (in *BooleanIntegerNumberString) Bool() bool {
+	return *in.bool
+}
+
+// Int64 returns the underlying int64. Int64 will panic if the underlying type is not int64
+func (in *BooleanIntegerNumberString) Int64() int64 {
+	return *in.int64
+}
+
+// Float64 returns the underlying float64. Float64 will panic if the underlying type is not float64
+func (in *BooleanIntegerNumberString) Float64() float64 {
+	return *in.float64
+}
+
+// String returns the underlying string. String will panic if the underlying type is not string
+func (in *BooleanIntegerNumberString) String() string {
+	return *in.string
+}
+
+func (in *BooleanIntegerNumberString) Value() any {
+	if in.IsBool() {
+		return *in.bool
+	} else if in.IsInt64() {
+		return *in.int64
+	} else if in.IsFloat64() {
+		return *in.float64
+	} else if in.IsString() {
+		return *in.string
+	}
+	return nil
+}
+
+func (in *BooleanIntegerNumberString) UnmarshalYAML(data []byte) error {
+	var b bool
+	if err := yaml.Unmarshal(data, &b); err == nil {
+		in.bool = &b
+		return nil
+	}
+
+	if bytes.Contains(data, []byte{'.'}) {
+		var f float64
+		if err := yaml.Unmarshal(data, &f); err != nil {
+			var s string
+			if err := yaml.Unmarshal(data, &s); err != nil {
+				return err
+			}
+			in.string = &s
+			return nil
+		}
+		in.float64 = &f
+		return nil
+	}
+
+	var i int64
+	if err := yaml.Unmarshal(data, &i); err != nil {
+		var s string
+		if err := yaml.Unmarshal(data, &s); err != nil {
+			return err
+		}
+		in.string = &s
+		return nil
+	}
+	in.int64 = &i
+	return nil
+}
+
+func (in *BooleanIntegerNumberString) MarshalYAML() ([]byte, error) {
+	return yaml.Marshal(in.Value())
+}
+
+func NewBooleanIntegerNumberString[T bool | int64 | float64 | string](v T) *BooleanIntegerNumberString {
+	switch val := any(v).(type) {
+	case bool:
+		return &BooleanIntegerNumberString{bool: &val}
+	case int64:
+		return &BooleanIntegerNumberString{int64: &val}
+	case float64:
+		return &BooleanIntegerNumberString{float64: &val}
+	case string:
+		return &BooleanIntegerNumberString{string: &val}
 	default:
 		panic(fmt.Sprintf("unexpected type %T", val))
 	}
--- yamlschema/types_test.go
+++ yamlschema/types_test.go
@@ -14,22 +14,34 @@ i2: 321
 f2: 3.21
 s1: "123"
 s2: "1.23"
+i3: 123
+f3: 1.23
+s3: "123"
+s4: "1.23"
+b1: false
+b2: true
 `
 
 var (
-	// TODO: include booleans once schema is fixed (see comment on IntegerNumberString.UnmarshalYAML
-	badIntNum    = []string{"1.1.1", "str", `"123"`, "[]", "{}", "\"map.key\": []"}
-	badIntNumStr = []string{"[]", "{}", "\"map.key\": []"}
+	badIntNum        = []string{"1.1.1", "str", `"123"`, "[]", "{}", "\"map.key\": []"}
+	badIntNumStr     = []string{"[]", "{}", "\"map.key\": []"}
+	badBoolIntNumStr = []string{"[]", "{}", "\"map.key\": []"}
 )
 
 func TestTypes(t *testing.T) {
 	type test struct {
-		I1 *schema.IntegerNumber       `yaml:"i1"`
-		F1 *schema.IntegerNumber       `yaml:"f1"`
-		I2 *schema.IntegerNumberString `yaml:"i2"`
-		F2 *schema.IntegerNumberString `yaml:"f2"`
-		S1 *schema.IntegerNumberString `yaml:"s1"`
-		S2 *schema.IntegerNumberString `yaml:"s2"`
+		I1 *schema.IntegerNumber              `yaml:"i1"`
+		F1 *schema.IntegerNumber              `yaml:"f1"`
+		I2 *schema.IntegerNumberString        `yaml:"i2"`
+		F2 *schema.IntegerNumberString        `yaml:"f2"`
+		S1 *schema.IntegerNumberString        `yaml:"s1"`
+		S2 *schema.IntegerNumberString        `yaml:"s2"`
+		I3 *schema.BooleanIntegerNumberString `yaml:"i3"`
+		F3 *schema.BooleanIntegerNumberString `yaml:"f3"`
+		S3 *schema.BooleanIntegerNumberString `yaml:"s3"`
+		S4 *schema.BooleanIntegerNumberString `yaml:"s4"`
+		B1 *schema.BooleanIntegerNumberString `yaml:"b1"`
+		B2 *schema.BooleanIntegerNumberString `yaml:"b2"`
 	}
 
 	testdata := &test{
@@ -39,6 +51,12 @@ func TestTypes(t *testing.T) {
 		F2: schema.NewIntegerNumberString(3.21),
 		S1: schema.NewIntegerNumberString("123"),
 		S2: schema.NewIntegerNumberString("1.23"),
+		I3: schema.NewBooleanIntegerNumberString[int64](123),
+		F3: schema.NewBooleanIntegerNumberString(1.23),
+		S3: schema.NewBooleanIntegerNumberString("123"),
+		S4: schema.NewBooleanIntegerNumberString("1.23"),
+		B1: schema.NewBooleanIntegerNumberString(false),
+		B2: schema.NewBooleanIntegerNumberString(true),
 	}
 
 	testcase := new(test)
@@ -70,6 +88,25 @@ func TestTypes(t *testing.T) {
 		t.Errorf("expected S2 to be equal: want %v, have %v (%[2]T)", testdata.S2.String(), testcase.S2.Value())
 	}
 
+	if !testcase.I3.IsInt64() || testcase.I3.Int64() != testdata.I3.Int64() {
+		t.Errorf("expected I3 to be equal: want %v, have %v (%[2]T)", testdata.I3.Int64(), testcase.I3.Value())
+	}
+	if !testcase.F3.IsFloat64() || testcase.F3.Float64() != testdata.F3.Float64() {
+		t.Errorf("expected F3 to be equal: want %v, have %v (%[2]T)", testdata.F3.Int64(), testcase.F3.Value())
+	}
+	if !testcase.S3.IsString() || testcase.S3.String() != testdata.S3.String() {
+		t.Errorf("expected S3 to be equal: want %v, have %v (%[2]T)", testdata.S3.String(), testcase.S3.Value())
+	}
+	if !testcase.S4.IsString() || testcase.S4.String() != testdata.S4.String() {
+		t.Errorf("expected S4 to be equal: want %v, have %v (%[2]T)", testdata.S4.String(), testcase.S4.Value())
+	}
+	if !testcase.B1.IsBool() || testcase.B1.Bool() != testdata.B1.Bool() {
+		t.Errorf("expected B2 to be equal: want %v, have %v (%[2]T)", testdata.B1.Bool(), testcase.B1.Bool())
+	}
+	if !testcase.B2.IsBool() || testcase.B2.Bool() != testdata.B2.Bool() {
+		t.Errorf("expected B2 to be equal: want %v, have %v (%[2]T)", testdata.B2.Bool(), testcase.B2.Bool())
+	}
+
 	buf, err := yaml.Marshal(testcase)
 	if err != nil {
 		t.Fatalf("could not marshal test case: %v", err)
@@ -97,6 +134,15 @@ func TestTypes(t *testing.T) {
 		}
 	}
 
+	{
+		nilcase := new(schema.BooleanIntegerNumberString)
+		if err := yaml.Unmarshal([]byte("null"), nilcase); err != nil {
+			t.Errorf("could not unmarshal BooleanIntegerNumberString null test case: %v", err)
+		} else if nilcase.Value() != nil {
+			t.Errorf("expected BooleanIntegerNumberString null to be nil: %#v", nilcase.Value())
+		}
+	}
+
 	for _, bad := range badIntNum {
 		testcase := new(schema.IntegerNumber)
 		if err := yaml.Unmarshal([]byte(bad), testcase); err == nil {
@@ -106,6 +152,13 @@ func TestTypes(t *testing.T) {
 
 	for _, bad := range badIntNumStr {
 		testcase := new(schema.IntegerNumberString)
+		if err := yaml.Unmarshal([]byte(bad), testcase); err == nil {
+			t.Errorf("expected IntegerNumberString test case to fail: %s (%#v)", bad, testcase.Value())
+		}
+	}
+
+	for _, bad := range badBoolIntNumStr {
+		testcase := new(schema.BooleanIntegerNumberString)
 		if err := yaml.Unmarshal([]byte(bad), testcase); err == nil {
 			t.Errorf("expected IntegerNumberString test case to fail: %s (%#v)", bad, testcase.Value())
 		}
